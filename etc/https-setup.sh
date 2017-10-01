d8d012a9bd282fd3a4481f97104cf88751b35677
Bo Henderson <bohende@gmail.com>
2017-09-30 18:56:15 -0700

    Separate nginx & https setup scripts

--- etc/https-setup.sh
+++ etc/https-setup.sh
@@ -11,6 +11,8 @@ dir=`dirname "$(readlink -f "$0")"`
 
 scp "$dir/nginx.conf" bjvm:~
 
+email=`git config user.email`
+
 ssh bjvm "bash -s" <<`EOF`
 
 
@@ -24,8 +26,35 @@ fi
 sed -i 's/server_name .*;/server_name '`hostname`';/' nginx.conf
 # sudo mv -f nginx.conf /etc/nginx/nginx.conf
 
+echo `hostname`
+exit
+
+## Install certbot if it's not installed already
+if ! hash certbot 2> /dev/null; then
+  sudo add-apt-repository ppa:certbot/certbot -y
+  sudo apt-get update -y
+  sudo apt-get install python-certbot-nginx -y
+
+  sudo certbot --nginx -d `hostname` <<EOIF
+$email
+A
+N
+2
+EOIF
+
+  sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+  sudo sed -i '0,/# managed by Certbot/ s/.*# managed by Certbot/        ssl_dhparam \/etc\/ssl\/certs\/dhparam.pem;\n&/' /etc/nginx/nginx.conf
+else
+
+# Reinstall existing certificates
+sudo certbot --nginx -d `hostname` <<EOIF
+1
+2
+EOIF
+
+fi
+
 # Set appropriate permissions
-# ???
 
 sudo nginx -t 2> /dev/null
 if [[ $? -eq 0 ]]; then
