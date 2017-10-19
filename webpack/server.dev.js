5100d335eef833ddbefe5e921d4ba6fdc0acc612
Bo Henderson <bohende@gmail.com>
2017-10-19 16:33:18 -0700

    Consolidate server webpack config

--- webpack/server.dev.js
+++ webpack/server.dev.js
@@ -5,7 +5,7 @@ const webpack = require('webpack')
 const precss = require('precss')
 const autoprefixer = require('autoprefixer')
 
-module.exports = merge(require('./server.common.js'), {
+module.exports = merge(require('./server.prod.js'), {
 
   // https://webpack.js.org/configuration/devtool/
   devtool: 'cheap-eval-source-map',
@@ -14,16 +14,6 @@ module.exports = merge(require('./server.common.js'), {
   output: { publicPath: '/' },
 
   recordsPath: path.join(__dirname, '../built/records'),
-
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-    ],
-  },
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
--- webpack/server.prod.js
+++ webpack/server.prod.js
@@ -1,8 +1,27 @@
 
-const merge = require('webpack-merge')
+const fs = require('fs')
+const path = require('path')
 const webpack = require('webpack')
 
-module.exports = merge(require('./server.common.js'), {
+const nodeModules = {}
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod
+  })
+
+module.exports = {
+
+  entry: {
+    server: './src/server.js',
+  },
+
+  output: {
+    path: path.join(__dirname, '../built'),
+    filename: '[name].bundle.js',
+  },
 
   module: {
     rules: [
@@ -14,4 +33,22 @@ module.exports = merge(require('./server.common.js'), {
     ],
   },
 
-});
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+
+  target: 'node',
+
+//  externals: nodeModules,
+
+  node: {
+    __dirname: true,
+    console: true,
+  },
+
+  plugins: [
+    new webpack.IgnorePlugin(/\.s?css$/),
+  ],
+
+}
+
