92435625bb9d78983560d83fe6a2ab13eef7dabc
Bo Henderson <bohende@gmail.com>
2017-11-21 18:04:33 -0800

    ops/secret-init used harden!

--- ops/secret-init.sh
+++ ops/secret-init.sh
@@ -5,8 +5,7 @@ secrets=("admin" "mongo" "geth")
 for sec in "${secrets[@]}"
 do
   echo -n "Looking for $sec... "
-
-  if [[ ! `docker secret ls -f name=$sec -q` ]]
+  if [[ -z "`docker secret ls | awk '{print$2}' | egrep "^$sec$"`" ]]
   then
     echo -n "Don't see it, initializing... "
     head -c30 /dev/urandom | base64 | tr -d '\n\r ' | docker secret create $sec - > /dev/null
