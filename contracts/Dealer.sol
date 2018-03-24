7394b2308b8c3312f2c907b63f56154d3bdddb04
Solid-Code <jmcgihon@gmail.com>
2018-03-24 12:28:15 -0700

    Implemented discussed changes.
        // Just a first crack at it. 
        
        // TODO: Add an address=>info mapping
       only info is the dealers address mapped to their balance
        
        // add signup function that creates a new "dealer"
        There is no need for a dealer to sign up.  
        The transaction call linked on their webpage will deposit it into thier account.
        They transfer eth with a deposit to start their bank roll.
        
        // style account with deposit/cashout events that
        modified deposit, created cashout and overflow events
        
        // the bjtj frontend connects to & backend listens for
        // events are per dealer account, gotta specify which dealer 
        DONE for the contract end
        
        // when depositing too. Ideally dealers couldn't cashout to
        // themselves but I see no way to enforce that..
        Agree

--- contracts/Dealer.sol
+++ contracts/Dealer.sol
@@ -1,44 +1,75 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.21;
 
-contract Dealer {
+contract thePit {
 
+    // Just a first crack at it. 
+    
     // TODO: Add an address=>info mapping
+    // only info is the dealers address mapped to their balance
+    
     // add signup function that creates a new "dealer"
+    // There is no need for a dealer to sign up.  
+    // The transaction call linked on their webpage will deposit it into thier account.
+    // They transfer eth with a deposit to start their bank roll.
+    
     // style account with deposit/cashout events that
+    // modified deposit, created cashout and overflow events
+    
     // the bjtj frontend connects to & backend listens for
-    // events are per dealer accout, gotta specify which dealer 
+    // events are per dealer account, gotta specify which dealer 
+    // DONE for the contract
+    
     // when depositing too. Ideally dealers couldn't cashout to
     // themselves but I see no way to enforce that..
+    // Agree
 
     address public pitboss;
-    uint public constant ceiling = 0.25 ether;
+    uint256 public constant ceiling = 0.25 ether;
 
-    event Deposit(address indexed _from, uint _value);
+    mapping(address => uint256) public bankrolls;
+    
+    event Deposit(address indexed _dealer, address indexed _depositor, uint _amount);
+    event Cashout(address indexed _dealer, address indexed _winner, uint _amount);
+    event Overflow(address indexed _dealer, uint256 _dealerAmount, address indexed _pitboss, uint256 _pitbossAmount);
 
-    function Dealer() public {
+    function thePit() public {
       pitboss = msg.sender;
     }
 
-    function () public payable {
-      Deposit(msg.sender, msg.value);
+    function deposit(address _dealer) public payable {
+      emit Deposit(_dealer, msg.sender, msg.value);
     }
 
-    modifier pitbossOnly {
-      require(msg.sender == pitboss);
+    modifier onlyBy(address _address) {
+      require(msg.sender == _address);
       _;
     }
 
-    function cashout(address winner, uint amount) public pitbossOnly {
-      // are cashout events worth it?
+    function cashout(address winner, uint amount) public {
+      require(amount <= ceiling);
+      uint256 dealerBankroll = bankrolls[msg.sender];
+      require(dealerBankroll >= amount);
+      if(dealerBankroll >= ceiling + 0.1 ether){
+        bankrolls[msg.sender] =- dealerBankroll - ceiling;
+        msg.sender.transfer((dealerBankroll - ceiling) * 9 / 10);
+        pitboss.transfer((dealerBankroll - ceiling) / 10);
+        emit Overflow(msg.sender, (dealerBankroll - ceiling) * 9 / 10, pitboss, (dealerBankroll - ceiling) / 10);
+      }
+      bankrolls[msg.sender] =- amount;
       winner.transfer(amount);
+      emit Cashout(msg.sender, winner, amount);
     }
 
-    function overflow() public pitbossOnly {
-      require (this.balance > ceiling);
-      pitboss.transfer(this.balance - ceiling);
+    function manualOverflow(address _dealer) public onlyBy(pitboss) {
+      uint256 dealerBankroll = bankrolls[_dealer];
+      require (dealerBankroll >= ceiling + 0.1 ether);
+      bankrolls[msg.sender] =- dealerBankroll - ceiling;
+      pitboss.transfer((dealerBankroll - ceiling) / 10);
+      _dealer.transfer((dealerBankroll - ceiling) * 9 / 10);
+      emit Overflow(msg.sender, (dealerBankroll - ceiling) * 9 / 10, pitboss, (dealerBankroll - ceiling) / 10);
     }
 
-    function kill() public pitbossOnly {
+    function kill() public onlyBy(pitboss) {
       selfdestruct(pitboss);
     }
 
