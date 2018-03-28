e908ff02a9b6a66de0b0730a8e379cc67406788a
Bo Henderson <bohende@gmail.com>
2018-03-28 12:43:20 -0700

    Init server-side signature validation

--- php/auth.php
+++ php/auth.php
@@ -1,7 +1,7 @@
 <?php
 
 include ABSPATH.'wp-content/plugins/bjtj/include/keccak.php';
-use kornrunner\Keccak;
+include ABSPATH.'wp-content/plugins/bjtj/include/recoverPublicKey.php';
 
 $agreement = 'I understand and agree that
 this game is an elaborate tip jar.
@@ -16,22 +16,37 @@ unwilling to refund my chips.
 // (ascii_string) => hex_string
 function keccak($ascii) {
   // cache results if we notice performance problems
-  return Keccak::hash($ascii, 256);
+  return kornrunner\Keccak::hash($ascii, 256);
 }
 
-function bjtj_hash_message($msg) {
-  // hash(b72472 . ea0d08) = 0xe2e5ae
-  return keccak( pack('H*', keccak('string Agreement').keccak($msg)));
+// This works just like eth-sig-util's typedSignatureHash
+// https://github.com/MetaMask/eth-sig-util/blob/master/index.js#L77
+function bjtj_hash_string($str) {
+  return keccak( pack('H*', keccak('string Agreement').keccak($str)));
 }
 
+// id for identification aka ethereum address
+// ag for autograph aka id's signature of above agreement
 function bjtj_auth($id, $ag) {
-
   global $agreement;
-  $agreement_hash = bjtj_hash_message($agreement);
 
-  //echo substr($agreement_hash, 0, 6);
+  // hash agreement & convert that hex string to Gnu Multi Precision
+  $agreement_gmp = gmp_init('0x'.bjtj_hash_string($agreement));
 
-  return true;
+  // extract r & s from the given signature & convert to GMP
+  $r = gmp_init('0x'.substr($ag, 0+2, 64));
+  $s = gmp_init('0x'.substr($ag, 64+2, 64));
+
+  // extract v from given signature & convert to int in range 0-4
+  $v = hexdec(substr($ag, 128+2, 2)) - 27;
+
+  // https://github.com/tuaris/CryptoCurrencyPHP
+  $pubKey = recoverPublicKey($r, $s, $agreement_gmp, $v);
+
+  // eth address is the last 40 chars of keccak of public key
+  $addr = '0x'.substr(keccak(pack('H*', $pubKey)), 24, 40);
+
+  return $addr == $id;
 }
 
 ?>
---
+++ php/include/recoverPublicKey.php
@@ -0,0 +1,452 @@
+<?php
+
+// This file contains bits & pieces copy/pasted from:
+// https://github.com/tuaris/CryptoCurrencyPHP
+
+/* 
+ * The SECp256k1 curve
+ * Fundamental ECC Function for Bitcoin/Zetacoin compatable Crypto Currency
+ * @author Daniel Morante
+ * Some parts may contain work based on Jan Moritz Lindemann, Matyas Danter, and Joey Hewitt
+ */
+
+class SECp256k1 {
+    public $a;
+    public $b;
+    public $p;
+    public $n;
+    public $G;
+    public function __construct(){
+        $this->a = gmp_init('0', 10);
+        $this->b = gmp_init('7', 10);
+        $this->p = gmp_init('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F', 16);
+        $this->n = gmp_init('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141', 16);
+        $this->G = array('x' => gmp_init('55066263022277343669578718895168534326250603453777594175500187360389116729240'),
+                         'y' => gmp_init('32670510020758816978083085130507043184471273380659243275938904335757337482424'));
+    }
+}
+
+/*
+ * Object orieted interface to Helpful Point Math Operations
+ * Using the GMP library
+ *
+ * @author Daniel Morante
+ * Some parts may contain work based on Jan Moritz Lindemann, Matyas Danter, and Joey Hewitt
+*/
+
+class PointMathGMP {
+
+    /***
+     * Computes the result of a point addition and returns the resulting point as an Array.
+     *
+     * @param Array $pt
+     * @return Array Point
+     * @throws \Exception
+     */
+    public static function doublePoint(Array $pt, $a, $p)
+    {
+        $gcd = gmp_strval(gmp_gcd(gmp_mod(gmp_mul(gmp_init(2, 10), $pt['y']), $p),$p));
+        if($gcd != '1')
+        {
+            throw new \Exception('This library doesn\'t yet supports point at infinity. See https://github.com/BitcoinPHP/BitcoinECDSA.php/issues/9');
+        }
+
+        // SLOPE = (3 * ptX^2 + a )/( 2*ptY )
+        // Equals (3 * ptX^2 + a ) * ( 2*ptY )^-1
+        $slope = gmp_mod(
+                         gmp_mul(
+                                 gmp_invert(
+                                            gmp_mod(
+                                                    gmp_mul(
+                                                            gmp_init(2, 10),
+                                                            $pt['y']
+                                                    ),
+                                                    $p
+                                            ),
+                                            $p
+                                 ),
+                                 gmp_add(
+                                         gmp_mul(
+                                                 gmp_init(3, 10),
+                                                 gmp_pow($pt['x'], 2)
+                                         ),
+                                         $a
+                                 )
+                         ),
+                         $p
+                );
+
+        // nPtX = slope^2 - 2 * ptX
+        // Equals slope^2 - ptX - ptX
+        $nPt = array();
+        $nPt['x'] = gmp_mod(
+                            gmp_sub(
+                                    gmp_sub(
+                                            gmp_pow($slope, 2),
+                                            $pt['x']
+                                    ),
+                                    $pt['x']
+                            ),
+                            $p
+                    );
+
+        // nPtY = slope * (ptX - nPtx) - ptY
+        $nPt['y'] = gmp_mod(
+                            gmp_sub(
+                                    gmp_mul(
+                                            $slope,
+                                            gmp_sub(
+                                                    $pt['x'],
+                                                    $nPt['x']
+                                            )
+                                    ),
+                                    $pt['y']
+                            ),
+                            $p
+                    );
+
+        return $nPt;
+    }
+
+    /***
+     * Computes the result of a point addition and returns the resulting point as an Array.
+     *
+     * @param Array $pt1
+     * @param Array $pt2
+     * @return Array Point
+     * @throws \Exception
+     */
+    public static function addPoints(Array $pt1, Array $pt2, $a, $p)
+    {
+        if(gmp_cmp($pt1['x'], $pt2['x']) == 0  && gmp_cmp($pt1['y'], $pt2['y']) == 0) //if identical
+        {
+            return self::doublePoint($pt1, $a, $p);
+        }
+
+        $gcd = gmp_strval(gmp_gcd(gmp_sub($pt1['x'], $pt2['x']), $p));
+        if($gcd != '1')
+        {
+            throw new \Exception('This library doesn\'t yet supports point at infinity. See https://github.com/BitcoinPHP/BitcoinECDSA.php/issues/9');
+        }
+
+        // SLOPE = (pt1Y - pt2Y)/( pt1X - pt2X )
+        // Equals (pt1Y - pt2Y) * ( pt1X - pt2X )^-1
+        $slope      = gmp_mod(
+                              gmp_mul(
+                                      gmp_sub(
+                                              $pt1['y'],
+                                              $pt2['y']
+                                      ),
+                                      gmp_invert(
+                                                 gmp_sub(
+                                                         $pt1['x'],
+                                                         $pt2['x']
+                                                 ),
+                                                 $p
+                                      )
+                              ),
+                              $p
+                      );
+
+        // nPtX = slope^2 - ptX1 - ptX2
+        $nPt = array();
+        $nPt['x']   = gmp_mod(
+                              gmp_sub(
+                                      gmp_sub(
+                                              gmp_pow($slope, 2),
+                                              $pt1['x']
+                                      ),
+                                      $pt2['x']
+                              ),
+                              $p
+                      );
+
+        // nPtX = slope * (ptX1 - nPtX) - ptY1
+        $nPt['y']   = gmp_mod(
+                              gmp_sub(
+                                      gmp_mul(
+                                              $slope,
+                                              gmp_sub(
+                                                      $pt1['x'],
+                                                      $nPt['x']
+                                              )
+                                      ),
+                                      $pt1['y']
+                              ),
+                              $p
+                      );
+
+        return $nPt;
+    }
+
+    /***
+     * Computes the result of a point multiplication and returns the resulting point as an Array.
+     *
+     * @param String Hex $k
+     * @param Array $pG (GMP, GMP)
+     * @param $base (INT)
+     * @throws \Exception
+     * @return Array Point (GMP, GMP)
+     */
+    public static function mulPoint($k, Array $pG, $a, $b, $p, $base = null)
+    {
+        //in order to calculate k*G
+        if($base == 16 || $base == null || is_resource($base))
+            $k = gmp_init($k, 16);
+        if($base == 10)
+            $k = gmp_init($k, 10);
+        $kBin = gmp_strval($k, 2);
+
+        $lastPoint = $pG;
+        for($i = 1; $i < strlen($kBin); $i++)
+        {
+            if(substr($kBin, $i, 1) == 1 )
+            {
+                $dPt = self::doublePoint($lastPoint, $a, $p);
+                $lastPoint = self::addPoints($dPt, $pG, $a, $p);
+            }
+            else
+            {
+                $lastPoint = self::doublePoint($lastPoint, $a, $p);
+            }
+        }
+        if(!self::validatePoint(gmp_strval($lastPoint['x'], 16), gmp_strval($lastPoint['y'], 16), $a, $b, $p)){
+            throw new \Exception('The resulting point is not on the curve.');
+    }
+        return $lastPoint;
+    }
+
+    /***
+     * Calculates the square root of $a mod p and returns the 2 solutions as an array.
+     *
+     * @param $a
+     * @return array|null
+     * @throws \Exception
+     */
+    public static function sqrt($a, $p)
+    {
+        if(gmp_legendre($a, $p) != 1)
+        {
+            //no result
+            return null;
+        }
+
+        if(gmp_strval(gmp_mod($p, gmp_init(4, 10)), 10) == 3)
+        {
+            $sqrt1 = gmp_powm(
+                            $a,
+                            gmp_div_q(
+                                gmp_add($p, gmp_init(1, 10)),
+                                gmp_init(4, 10)
+                            ),
+                            $p
+                    );
+            // there are always 2 results for a square root
+            // In an infinite number field you have -2^2 = 2^2 = 4
+            // In a finite number field you have a^2 = (p-a)^2
+            $sqrt2 = gmp_mod(gmp_sub($p, $sqrt1), $p);
+            return array($sqrt1, $sqrt2);
+        }
+        else
+        {
+            throw new \Exception('P % 4 != 3 , this isn\'t supported yet.');
+        }
+    }
+
+    /***
+     * Calculate the Y coordinates for a given X coordinate.
+     *
+     * @param $x
+     * @param null $derEvenOrOddCode
+     * @return array|null|String
+     */
+    public static function calculateYWithX($x, $a, $b, $p, $derEvenOrOddCode = null)
+    {
+        $x  = gmp_init($x, 16);
+        $y2 = gmp_mod(
+                      gmp_add(
+                              gmp_add(
+                                      gmp_powm($x, gmp_init(3, 10), $p),
+                                      gmp_mul($a, $x)
+                              ),
+                              $b
+                      ),
+                      $p
+              );
+
+        $y = self::sqrt($y2, $p);
+
+        if(!$y) //if there is no result
+        {
+            return null;
+        }
+
+        if(!$derEvenOrOddCode)
+        {
+            return $y;
+        }
+
+        else if($derEvenOrOddCode == '02') // even
+        {
+            $resY = null;
+            if(false == gmp_strval(gmp_mod($y[0], gmp_init(2, 10)), 10))
+                $resY = gmp_strval($y[0], 16);
+            if(false == gmp_strval(gmp_mod($y[1], gmp_init(2, 10)), 10))
+                $resY = gmp_strval($y[1], 16);
+            if($resY)
+            {
+                while(strlen($resY) < 64)
+                {
+                    $resY = '0' . $resY;
+                }
+            }
+            return $resY;
+        }
+        else if($derEvenOrOddCode == '03') // odd
+        {
+            $resY = null;
+            if(true == gmp_strval(gmp_mod($y[0], gmp_init(2, 10)), 10))
+                $resY = gmp_strval($y[0], 16);
+            if(true == gmp_strval(gmp_mod($y[1], gmp_init(2, 10)), 10))
+                $resY = gmp_strval($y[1], 16);
+            if($resY)
+            {
+                while(strlen($resY) < 64)
+                {
+                    $resY = '0' . $resY;
+                }
+            }
+            return $resY;
+        }
+
+        return null;
+    }
+
+    /***
+     * Returns true if the point is on the curve and false if it isn't.
+     *
+     * @param $x
+     * @param $y
+     * @return bool
+     */
+    public static function validatePoint($x, $y, $a, $b, $p)
+    {
+        $x  = gmp_init($x, 16);
+        $y2 = gmp_mod(
+                        gmp_add(
+                            gmp_add(
+                                gmp_powm($x, gmp_init(3, 10), $p),
+                                gmp_mul($a, $x)
+                            ),
+                            $b
+                        ),
+                        $p
+                    );
+        $y = gmp_mod(gmp_pow(gmp_init($y, 16), 2), $p);
+
+        if(gmp_cmp($y2, $y) == 0)
+            return true;
+        else
+            return false;
+    }
+
+    /***
+     * Returns Negated Point (Y).
+     *
+     * @param $point Array(GMP, GMP)
+     * @return Array(GMP, GMP)
+     */
+  public static function negatePoint($point) { 
+    return array('x' => $point['x'], 'y' => gmp_neg($point['y'])); 
+  }
+
+  // These 2 function don't really belong here.
+
+  // Checks is the given number (DEC String) is even
+  public static function isEvenNumber($number) {
+    return (((int)$number[strlen($number)-1]) & 1) == 0;
+  }
+  // Converts BIN to GMP
+  public static function bin2gmp($binStr) {
+    $v = gmp_init('0');
+
+    for ($i = 0; $i < strlen($binStr); $i++) {
+      $v = gmp_add(gmp_mul($v, 256), ord($binStr[$i]));
+    }
+
+    return $v;
+  }
+
+}
+
+/* 
+ * Crypto Currency Message Signing and Verification
+ * For Bitcoin/Zetacoin compatable Crypto Currency utilizing the secp256k1 curve
+ * @author Daniel Morante
+ * Some parts may contain work based on Jan Moritz Lindemann, Matyas Danter, and Joey Hewitt
+ */
+
+// $R, $S, and $hash are GMP
+// $recoveryFlags is INT
+function recoverPublicKey($R, $S, $hash, $recoveryFlags){
+  $secp256k1 = new SECp256k1();
+  $a = $secp256k1->a;
+  $b = $secp256k1->b;
+  $G = $secp256k1->G;
+  $n = $secp256k1->n;
+  $p = $secp256k1->p;
+  $isYEven = ($recoveryFlags & 1) != 0;
+  $isSecondKey = ($recoveryFlags & 2) != 0;
+  // PointMathGMP::mulPoint wants HEX String
+  $e = gmp_strval($hash, 16);
+  $s = gmp_strval($S, 16);
+  // Precalculate (p + 1) / 4 where p is the field order
+  // $p_over_four is GMP
+  static $p_over_four; // XXX just assuming only one curve/prime will be used
+  if (!$p_over_four) {
+    $p_over_four = gmp_div(gmp_add($p, 1), 4);
+  }
+  // 1.1 Compute x
+  // $x is GMP
+  if (!$isSecondKey) {
+    $x = $R;
+  } else {
+    $x = gmp_add($R, $n);
+  }
+  // 1.3 Convert x to point
+  // $alpha is GMP
+  $alpha = gmp_mod(gmp_add(gmp_add(gmp_pow($x, 3), gmp_mul($a, $x)), $b), $p);
+  // $beta is DEC String (INT)
+  $beta = gmp_strval(gmp_powm($alpha, $p_over_four, $p));
+  // If beta is even, but y isn't or vice versa, then convert it,
+  // otherwise we're done and y == beta.
+  if (PointMathGMP::isEvenNumber($beta) == $isYEven) {
+    // gmp_sub function will convert the DEC String "$beta" into a GMP
+    // $y is a GMP 
+    $y = gmp_sub($p, $beta);
+  } else {
+    // $y is a GMP
+    $y = gmp_init($beta);
+  }
+  // 1.4 Check that nR is at infinity (implicitly done in construtor) -- Not reallly
+  // $Rpt is Array(GMP, GMP)
+  $Rpt = array('x' => $x, 'y' => $y);
+  // 1.6.1 Compute a candidate public key Q = r^-1 (sR - eG)
+  // $rInv is a HEX String
+  $rInv = gmp_strval(gmp_invert($R, $n), 16);
+  // $eGNeg is Array (GMP, GMP)
+  $eGNeg = PointMathGMP::negatePoint(PointMathGMP::mulPoint($e, $G, $a, $b, $p));
+  $sR = PointMathGMP::mulPoint($s, $Rpt, $a, $b, $p);
+  $sR_plus_eGNeg = PointMathGMP::addPoints($sR, $eGNeg, $a, $p);
+  // $Q is Array (GMP, GMP)
+  $Q = PointMathGMP::mulPoint($rInv, $sR_plus_eGNeg, $a, $b, $p);
+  // Q is the derrived public key
+  // $pubkey is Array (HEX String, HEX String)
+  // Ensure it's always 64 HEX Charaters
+      $pubKey['x'] = str_pad(gmp_strval($Q['x'], 16), 64, 0, STR_PAD_LEFT);
+      $pubKey['y'] = str_pad(gmp_strval($Q['y'], 16), 64, 0, STR_PAD_LEFT);
+
+  return $pubKey['x'].$pubKey['y'];
+}
+
+?>
